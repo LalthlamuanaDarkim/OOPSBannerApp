a13bcb36dac6c782f5c1ab818224152ee7a253dd
LalthlamuanaDarkim <ld5010@srmist.edu.in>
2026-03-01 22:26:42 +0530

    Add files via upload

---
+++ OOPSBannerAppToo.java
@@ -0,0 +1,13 @@
+public class OOPSBannerAppToo {
+    public static void main(String[] args) {
+
+			System.out.println("  *****  " + "  *****  " + "  *****   " + "  *****   ");
+			System.out.println(" **   ** " + " **   ** " + " **   **  " + " **       ");
+			System.out.println("**     **" + "**     **" + "**     ** " + "**        ");
+			System.out.println("**     **" + "**     **" + "*******   " + "  *****   ");
+			System.out.println("**     **" + "**     **" + "**        " + "       ** ");
+			System.out.println(" **   ** " + " **   ** " + "**        " + "**    **  ");
+			System.out.println("  *****  " + "  *****  " + "**        " + "  *****   ");
+			
+		}
+	}
